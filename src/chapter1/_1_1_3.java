76839d7db8d9fcece943816eeac44793b52caa19
xiaojia23 <18120954@bjtu.edu.cn>
2020-01-30 11:19:57 +0800

    1.1.3 解答

---
+++ src/chapter1/_1_1_3.java
@@ -0,0 +1,19 @@
+package chapter1;
+
+import java.security.InvalidParameterException;
+
+public class _1_1_3 {
+    public static void main(String[] args){
+        if(args.length != 3){
+            System.out.println(args.length);
+            throw new InvalidParameterException();
+        }
+        if(Integer.parseInt(args[0]) == Integer.parseInt(args[1]) &&
+            Integer.parseInt(args[1]) == Integer.parseInt(args[2])){
+            System.out.println("equal");
+        }
+        else{
+            System.out.println("not equal");
+        }
+    }
+}
