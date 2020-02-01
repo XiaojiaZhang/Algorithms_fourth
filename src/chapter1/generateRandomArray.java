64028ef04fa5716fd6b4046fc71a8a42b5fb12f3
xiaojia23 <18120954@bjtu.edu.cn>
2020-02-01 13:12:53 +0800

    添加用于生成随机double数组的类相关方法

---
+++ src/chapter1/generateRandomArray.java
@@ -0,0 +1,26 @@
+package chapter1;
+
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class generateRandomArray {
+    public static double[] generate(int N, double low, double high){
+        double[] array = new double[N];
+        for(int i=0; i < N; i++){
+            array[i] = StdRandom.uniform(low, high);
+        }
+        return array;
+    }
+
+    public static void displayArray(double[] array){
+        for(int i=0; i < array.length; i++){
+            StdOut.printf("%f\t", array[i]);
+        }
+        StdOut.println();
+    }
+
+    public static void main(String[] args){
+        double[] array = generate(100,0.0, 1.0);
+        displayArray(array);
+    }
+}
