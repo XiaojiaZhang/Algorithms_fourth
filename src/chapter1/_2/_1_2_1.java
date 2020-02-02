39b6ce8bfd4252dfd7e83297f69f73acffcb2425
XiaojiaZhang <xiaojiazhang96@gmail.com>
2020-02-02 17:11:50 +0800

    创建Point2D类，根据Point2D API计算平面上各点对最大距离

--- src/chapter1/_2/_1_2_1.java
+++ src/chapter1/_2/_1_2_1.java
@@ -1,23 +1,55 @@
 package chapter1._2;
 
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.awt.*;
+
 public class _1_2_1 {
-    private static double distPoints(double[] a, double[] b){
-        return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+    public static class Point2D{
+        private double x, y;
+        public Point2D(double x, double y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public double x(){ return x; }
+
+        public double y(){ return y; }
+
+        //极径（极坐标)
+        public double r(){
+            return Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
+        }
+
+        //极角(极坐标)
+        public double theta(){
+            double acos = Math.acos(x / r());
+            if (y > 0){ return acos; }
+            else { return 2*Math.PI - acos; }
+        }
+
+        double distTo(Point2D that){
+            return Math.sqrt(Math.pow(x - that.x, 2) + Math.pow(y - that.y, 2));
+        }
+
+        void draw(){
+            StdDraw.point(x, y);
+        }
     }
 
+
     public static double maxDist(int N){
-        double[][] points = new double[N][2];
+        Point2D[] points = new Point2D[N];
         for(int i = 0; i < N; i++){
-            points[i][0] = StdRandom.uniform(0.0, 1.0);
-            points[i][1] = StdRandom.uniform(0.0, 1.0);
+            points[i] = new Point2D(StdRandom.uniform(0.0, 1.0),
+                    StdRandom.uniform(0.0, 1.0));
         }
         double max_dist = 0.0;
         for(int i = 0; i < N; i++){
             for(int j = i+1; j < N; j++){
-                double cur_dist = distPoints(points[i], points[j]);
+                double cur_dist = points[i].distTo(points[j]);
                 if(cur_dist > max_dist){
                     max_dist = cur_dist;
                 }
