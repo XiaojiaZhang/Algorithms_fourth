53168726e4154bf1b58e1481a34e06c0923e436f
XiaojiaZhang <xiaojiazhang96@gmail.com>
2020-02-03 12:09:19 +0800

    1.2.13 解答

---
+++ src/chapter1/_2/_1_2_13.java
@@ -0,0 +1,68 @@
+package chapter1._2;
+
+public class _1_2_13 {
+    public static class Date{
+        private final int month;
+        private final int day;
+        private final int year;
+        public Date(int m, int d, int y){
+            month = m;
+            day = d;
+            year = y;
+        }
+
+        public int month(){
+            return month;
+        }
+        public int year(){
+            return year;
+        }
+        public int day(){
+            return day;
+        }
+
+        public String toString(){
+            return month + "/" + day + "/" + year;
+        }
+
+        public boolean equals(Object x){
+            if(this == x) return true;
+            if(x == null) return false;
+            if(this.getClass() != x.getClass()) return false;
+            Date that = (Date)x;
+            if(this.day != that.day) return false;
+            if(this.month!= that.month) return false;
+            if(this.year!= that.year) return false;
+            return true;
+        }
+    }
+
+    public static class Transaction{
+        private String who;
+        private Date when;
+        private double amount;
+        public Transaction(String who, Date when, double amount){
+            this.who = who;
+            this.when = when;
+            this.amount = amount;
+        }
+
+        public String who(){ return who; }
+        public Date when(){ return when; }
+        public double amount(){ return amount; }
+
+        public String toString(){
+            return "who:" + who + "\t" + "when:" + "\t" + "amount:" + amount;
+        }
+
+        public boolean equals(Object that){
+            if(this == that) return true;
+            if(this.getClass() != that.getClass()) return false;
+            Transaction that_ = (Transaction)that;
+            if(!who.equals(that_.who)) return false;
+            if(!when.equals(that_.when)) return false;
+            if(Math.abs(amount - that_.amount) > 0.00001) return false;
+            return true;
+        }
+    }
+}
