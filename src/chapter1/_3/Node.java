889886593f20ae3204f809068cc34c25ab4521a4
XiaojiaZhang <xiaojiazhang96@gmail.com>
2020-02-05 10:56:00 +0800

    添加getItem和getNext方法用于获取Node对象的数据域

--- src/chapter1/_3/Node.java
+++ src/chapter1/_3/Node.java
@@ -5,12 +5,21 @@ import edu.princeton.cs.algs4.StdOut;
 public class Node<Item> {
     private Item item;
     private Node<Item> next;
+
     public Node(Item item){
         this.item = item;
     }
     public Node(Item item, Node<Item> next){
         this.item = item;
         this.next = next;
+    }
+
+    public Node<Item> getNext(){
+        return next;
+    }
+
+    public Item getItem(){
+        return item;
     }
 
     public void display(){
