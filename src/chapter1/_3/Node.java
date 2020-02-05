5e9006f4a735e6661aecccade9f209abe25980db
XiaojiaZhang <xiaojiazhang96@gmail.com>
2020-02-05 10:52:30 +0800

    添加Node类用于其他习题解答中的辅助类

---
+++ src/chapter1/_3/Node.java
@@ -0,0 +1,27 @@
+package chapter1._3;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class Node<Item> {
+    private Item item;
+    private Node<Item> next;
+    public Node(Item item){
+        this.item = item;
+    }
+    public Node(Item item, Node<Item> next){
+        this.item = item;
+        this.next = next;
+    }
+
+    public void display(){
+        Node<Item> current = this;
+        while (current != null){
+            StdOut.print(current.item);
+            if(current.next != null){
+                StdOut.print("->");
+            }
+            current = current.next;
+        }
+        StdOut.println();
+    }
+}
