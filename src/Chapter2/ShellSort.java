aeb6afdab56b4321c1f9b1f61e55445a3997bbc4
XiaojiaZhang <xiaojiazhang96@gmail.com>
2020-02-09 14:04:24 +0800

    添加希尔排序可视化方法

--- src/Chapter2/ShellSort.java
+++ src/Chapter2/ShellSort.java
@@ -1,5 +1,6 @@
 package Chapter2;
 
+import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
@@ -18,14 +19,35 @@ public class ShellSort extends ArraySort{
         }
     }
 
+    private static void sortShow(double[] a){
+        int N = a.length;
+        double[] minmax = DrawArray.getMinMax(a);
+        int h = 1;
+        while (h < N / 3) h = 3*h;
+        while (h >= 1){
+            for(int i = h; i < N; i++){
+                for(int j = i; j >= h && less(a[j], a[j-h]); j-=h){
+                    double tmp = a[j];
+                    a[j] = a[j-h];
+                    a[j-h] = tmp;
+                }
+            }
+            h /= 3;
+            DrawArray.drawArray(a, minmax[0], minmax[1]);
+            StdDraw.pause(100);
+            StdDraw.clear();
+        }
+        DrawArray.drawArray(a, minmax[0], minmax[1]);
+    }
+
     public static void main(String[] args){
-        int N = 10;
-        Double[] array = new Double[N];
+        int N = 100;
+        double[] array = new double[N];
         for(int i=0; i < N; i++)
             array[i] = StdRandom.uniform(-100.0, 100.0);
-        sort(array);
-        for(double d: array)
-            StdOut.println(d);
-        assert isSorted(array);
+        sortShow(array);
+//        for(double d: array)
+//            StdOut.println(d);
+//        assert isSorted(array);
     }
 }
